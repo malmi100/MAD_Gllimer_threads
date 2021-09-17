5e9ac4d993faac85a35d88d775d851c9c7e115d1
maneesha <maneeshalakshani9@gmail.com>
2021-09-17 23:55:51 +0530

    First-commit-by-Maneesha

--- app/src/main/java/com/example/mad_gllimer_threads/MainActivity.java
+++ app/src/main/java/com/example/mad_gllimer_threads/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    yhhhhhhhhhhhhh
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
