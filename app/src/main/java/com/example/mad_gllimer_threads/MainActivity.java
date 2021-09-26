57be759cbcc9a30579ba3a946277adbfa3c1a4b6
malmi100 <pehesarasamarawickrama@gmail.com>
2021-09-26 00:53:24 -0700

    Rating.xml read method

--- app/src/main/java/com/example/mad_gllimer_threads/MainActivity.java
+++ app/src/main/java/com/example/mad_gllimer_threads/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Pehesara");
+        System.out.println("Pehesara");
     }
 }
